e19e556c361068c35663b4be333c0063b5fdf10b
Jonathan6026 <66056467+Jonathan6026@users.noreply.github.com>
2020-08-19 09:13:18 +0800

    更新了顶部导航栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,29 @@
 <template>
-  <div>
-    <h2>首页</h2>
-  </div>
+    <div id="home">
+      <nav-bar class="home-nav-bar">
+            <div slot="center">购物街</div>
+          </nav-bar>
+    </div>
 </template>
 
 <script>
+  import navBar from '../../components/common/navbar/navBar.vue'
   export default {
     name:'Home',
     data () {
       return {
-        
+
       }
+    },
+    components: {
+      navBar
     }
   }
 </script>
 
-<style>
+<style scoped>
+  .home-nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
